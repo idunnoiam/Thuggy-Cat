8fb801b7ed9131bd5780eab0c59e4469abb449e3
idunnoiam <74386219+idunnoiam@users.noreply.github.com>
2021-10-24 14:31:32 -0800

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -1,3 +1,6 @@
+user_name = localStorage.getItem("user_name");
+document.getElementById("user_name").innerHTML = "Welcome " + user_name + "!";
+
 const firebaseConfig = {
     apiKey: "AIzaSyAncFNlICfkshqQ6UPb4lzXeVCaqQsM4Ik",
     authDomain: "kwitter-8623d.firebaseapp.com",
@@ -19,9 +22,28 @@ function getData() {
             childKey = childSnapshot.key;
             Room_names = childKey;
             //Start code
-
+            console.log("Room Name - " + Room_names);
+            row = "<div class='room_name' id=" + Room_names + " onclick='redirectToRoomName(this.id)' >#" + Room_names + "</div><hr>";
+            document.getElementById("output").innerHTML += row;
             //End code
         });
     });
 }
 getData();
+
+function addRoom() {
+    room_name = document.getElementById("room_name").value;
+
+    firebase.database().ref("/").child(room_name).update({
+        purpose: "adding room name"
+    });
+    localStorage.setItem("room_name", room_name);
+
+    window.location = "kwitter_page.html";
+}
+
+function redirectToRoomName(name) {
+    console.log(name);
+    localStorage.setItem("room_name", name);
+    window.location = "kwitter_page.html";
+}
